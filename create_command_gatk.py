23c3a74970112ae6e913f82818bd1a2103babdd8
alba <76627046+albacamino@users.noreply.github.com>
2023-02-08 14:07:33 +0100

    Update create_command_gatk.py

--- create_command_gatk.py
+++ create_command_gatk.py
@@ -1,18 +1,18 @@
 import os 
 
-list_dir = os.listdir()
-list_fpies = os.listdir("C:/Users/USUARIO/Desktop/FPIES/Total_FPIES/total_fpies_assoc")
+list_dir = os.listdir("path/to/dir")
+list_fpies = os.listdir("path/to/dir")
 
 comando  = "gatk CombineGVCFs -R /home/common/references/hg38/Homo_sapiens_assembly38.fasta -D /home/common/references/hg38/Homo_sapiens_assembly38.dbsnp138.vcf.gz "
 tail = "-O khor_fpies.g.vcf.gz"
 
 for file in list_dir:
     if not file.endswith(".tgi"):
-        flag = ("--variant"+ " gvcfs/"+ file+" ")
+        flag = ("--variant"+ " path/to/dir"+ file+" ")
         comando = comando + flag
 for file in list_fpies:
     if not file.endswith((".pdf",".txt")):
-        flag = ("--variant"+ " C:/Users/USUARIO/Desktop/FPIES/Total_FPIES/total_fpies_assoc/"+ file+" ")
+        flag = ("--variant"+ " path/to/dir"+ file+" ")
         comando = comando + flag
 comando = comando + tail
 print(comando)
