fc3f554169934076ef432f0b018200e817c09e5b
Alba Camino <albac.mera@gmail.com>
2023-02-08 13:59:40 +0100

    Added create command script

---
+++ create_command_gatk.py
@@ -0,0 +1,20 @@
+import os 
+
+list_dir = os.listdir("path/to/dir")
+list_fpies = os.listdir("path/to/dir")
+
+comando  = "gatk CombineGVCFs -R /home/common/references/hg38/Homo_sapiens_assembly38.fasta -D /home/common/references/hg38/Homo_sapiens_assembly38.dbsnp138.vcf.gz "
+tail = "-O output.vcf.gz"
+
+for file in list_dir:
+    if not file.endswith(".tbi"):
+        flag = ("--variant" + " path/to/dir/" + file + " ")
+        comando = comando + flag
+
+for file in list_fpies:
+    if not file.endswith((".pdf",".txt")):
+        flag = ("--variant" + " path/to/dir/" + file + " ")
+        comando = comando + flag
+comando = comando + tail
+print(comando)
+
